a89c7f424d6f0c274dfd73e22243381a6ae88778
xaldarof <temuruz2003@gmail.com>
2022-07-04 17:57:57 +0500

    1

---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,17 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <charset_converter/charset_converter_plugin.h>
+#include <network_info_plus_windows/network_info_plus_windows_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  CharsetConverterPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("CharsetConverterPlugin"));
+  NetworkInfoPlusWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("NetworkInfoPlusWindowsPlugin"));
+}
